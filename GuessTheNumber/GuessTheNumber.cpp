14ca211632370235bd51b41b5d07f36e6c73d595
Yuval Kogan <Yuvalkogan123@gmail.com>
2024-09-24 22:57:52 +0300

    Finish project - Guess The Number

---
+++ GuessTheNumber/GuessTheNumber.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+	int randomNumber;
+	int upperLimit;
+	int guess;
+
+	cout << "Pick an integer:\n" <<
+			"The integer must be greater than 0.\n";
+	
+	cin >> upperLimit; 
+	randomNumber = rand() % upperLimit;
+
+	cout << "Pick a number between 0 to " << upperLimit << endl;
+
+	for (int i = 5; i > 0;) {
+		cin >> guess; // Insert user`s guess.
+		cout << endl;
+
+		if (guess == randomNumber) {
+			cout << "Congratsulations, YOU WIN!\n";
+			return 0;
+		}
+
+		cout << "Wrong pick, tries left:" << --i << endl;
+	}
+
+	cout << "Better luck next time :)\n";
+	return 0;
+}
+
